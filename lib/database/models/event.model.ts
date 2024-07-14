c611cb61161dbcc540f6144727faba831c1449ac
AbdElRahman KahlOut <akahlouts@gmail.com>
2024-07-14 03:40:26 +0300

    fixed..

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -17,11 +17,11 @@ export interface IEvent extends Document {
 }
 
 const EventSchema = new Schema({
-  title: { title: String },
+  title: { type: String, required: true },
   description: { type: String },
   location: { type: String },
   createdAt: { type: Date, default: Date.now },
-  imageUrl: { type: String },
+  imageUrl: { type: String, required: true },
   startDateTime: { type: Date, default: Date.now },
   endDateTime: { type: Date, default: Date.now },
   price: { type: String },
