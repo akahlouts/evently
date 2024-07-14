14806a07a4ab8166175a8ddf1ba9844ef029dec9
AbdElRahman KahlOut <akahlouts@gmail.com>
2024-07-15 01:32:32 +0300

    Implement the functionality of adding category in db.

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -1,4 +1,9 @@
-import React, { startTransition, useState } from "react";
+import React, { startTransition, useEffect, useState } from "react";
+
+import {
+  createCategory,
+  getAllCategories,
+} from "@/lib/actions/category.actions";
 
 import {
   Select,
@@ -31,7 +36,21 @@ const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
   const [categories, setCategories] = useState<ICategory[]>([]);
   const [newCategory, setNewCategory] = useState("");
 
-  const handleAddCategory = () => {};
+  const handleAddCategory = () => {
+    createCategory({ categoryName: newCategory.trim() }).then((category) =>
+      setCategories((prevState) => [...prevState, category])
+    );
+  };
+
+  useEffect(() => {
+    const getCategories = async () => {
+      const categoryList = await getAllCategories();
+
+      categoryList && setCategories(categoryList as ICategory[]);
+    };
+
+    getCategories();
+  }, []);
 
   return (
     <Select onValueChange={onChangeHandler} defaultValue={value}>
@@ -52,7 +71,7 @@ const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
 
         <AlertDialog>
           <AlertDialogTrigger className="p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500">
-            Open
+            Add new category
           </AlertDialogTrigger>
           <AlertDialogContent className="bg-white">
             <AlertDialogHeader>
